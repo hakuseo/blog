0e66e89718d84992bccea7b409bbc65dc18d1d53
hakuseo <hakuseo@gmail.com>
2022-09-08 20:59:32 +0900

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+const accdianHome = document.querySelector('.accodian-home');
+const hiddenMenu = document.querySelector('.hidden-menu');
+
+console.log(hiddenMenu.style);
+accdianHome.addEventListener('click', (e) => {
+  // if (hiddenMenu.style.display == 'none') {
+  //   for (let i = 0; i < hiddenMenu.length; i++) {
+  //     console.log(i);
+  //   }
+  // }
+});
